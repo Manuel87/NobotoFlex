ed3e37b799f58f439989f4d81c849b2897b742ed
Manuel von Gebhardi <manuelvongebhardi@Manuels-MacBook-Pro.fritz.box>
2017-08-15 18:17:16 +0200

    Name

--- 2-build/build.sh
+++ 2-build/build.sh
@@ -1,14 +1,14 @@
 # Before use, make sure you have exported all necessary UFOs from GlYPHS (to temp/ufo)
 
 # Generate .designspace file, as writing it by hand is difficult for larger projects
-python RobotoFlex_DesignSpace.py
+python NobotoFlex_DesignSpace.py
 
 # Generate separate interpolatable TTFs # VERY SLOW, consider to comment it out, when you dont need
 #fontmake -o ttf-interpolatable -m RobotoFlex.designspace --no-production-names --subset --keep-overlaps --keep-direction
 
 
 # Generate Variable Font
-fonttools varLib RobotoFlex.designspace
+fonttools varLib NobotoFlex.designspace
 
 # Move to main Folder
-mv RobotoFlex-VF.ttf ../RobotoFlex-VF.ttf
+mv NobotoFlex-VF.ttf ../NobotoFlex-VF.ttf
